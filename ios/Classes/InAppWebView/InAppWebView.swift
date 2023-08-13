cdd1f773c71f6d6f2aaa0d59a8c9e2089237d9e6
Cuong Truong <truonglevancuong92@gmail.com>
2023-08-13 21:14:27 +0700

    - Force to use audio speaker - Grant permission one time

--- ios/Classes/InAppWebView/InAppWebView.swift
+++ ios/Classes/InAppWebView/InAppWebView.swift
@@ -8,6 +8,7 @@
 import Flutter
 import Foundation
 import WebKit
+import AVFoundation
 
 public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler, UIGestureRecognizerDelegate, PullToRefreshDelegate {
 
@@ -77,6 +78,7 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate, WKNavi
         panGestureRecognizer = UIPanGestureRecognizer()
         panGestureRecognizer.delegate = self
         panGestureRecognizer.addTarget(self, action: #selector(endDraggingDetected))
+        self.forceUsingSpeakerAudio()
     }
     
     override public var frame: CGRect {
@@ -101,6 +103,17 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate, WKNavi
     
     required public init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
+    }
+
+    private func forceUsingSpeakerAudio(){
+        let session: AVAudioSession = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(AVAudioSession.Category.playAndRecord)
+            try session.overrideOutputAudioPort(AVAudioSession.PortOverride.speaker)
+            try session.setActive(true)
+        } catch {
+            print("Couldn't override output audio port")
+        }
     }
 
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
@@ -1614,6 +1627,18 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate, WKNavi
         
         inAppBrowserDelegate?.didStartNavigation(url: url)
     }
+
+
+    @available(iOS 15, *)
+    public func webView(
+        _ webView: WKWebView,
+        requestMediaCapturePermissionFor origin: WKSecurityOrigin,
+        initiatedByFrame frame: WKFrameInfo,
+        type: WKMediaCaptureType,
+        decisionHandler: @escaping (WKPermissionDecision) -> Void
+    ) {
+        decisionHandler(.grant)
+    }
     
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         initializeWindowIdJS()
@@ -3108,4 +3133,3 @@ class DecisionHandlerCrashAvoider {
         deinitialize()
     }
 }
-
